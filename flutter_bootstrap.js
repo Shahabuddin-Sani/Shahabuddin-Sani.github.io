12d6588484d2e5a5d3cfa3403a249ddcc9927d98
Shahabuddin Sani <sani.shahabuddin@gmail.com>
2025-01-23 02:43:31 +0600

    third commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2578121387"
+    serviceWorkerVersion: "4154333758"
   }
 });
